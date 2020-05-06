b486f0e7899e53d2bb2dc328113767fd4b30c956
Daniela <danielaoancea2009@yahoo.ca>
2020-05-06 14:56:52 +0000

    text

---
+++ ingredients.js
@@ -0,0 +1,7 @@
+const ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+  var i = 0;
+   while (i < ingredients.length) {
+console.log(ingredients[i]);
+   i++;
+}
+
